0370d8d83c9cbf15d48ea6c5de642a9aa9ead638
PillageDev <63556914+PillageDev@users.noreply.github.com>
2024-01-22 20:03:16 -0500

    Initial Commit

---
+++ LoyaltyApp/LoyaltyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoyaltyAppApp.swift
+//  LoyaltyApp
+//
+//  Created by Justin Edgington on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LoyaltyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoyaltyAppUITests/LoyaltyAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoyaltyAppUITestsLaunchTests.swift
+//  LoyaltyAppUITests
+//
+//  Created by Justin Edgington on 1/22/24.
+//
+
+import XCTest
+
+final class LoyaltyAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
